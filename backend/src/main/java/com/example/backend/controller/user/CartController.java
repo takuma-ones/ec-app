8551e14e15abc5257b98702925472f919f305164
Takuma Sato <takuma.sato@onespower.com>
2025-06-09 17:25:29 +0900

    各コントローラーにメソッドのプレースホルダーを追加し、今後の実装に備える

--- backend/src/main/java/com/example/backend/controller/user/CartController.java
+++ backend/src/main/java/com/example/backend/controller/user/CartController.java
@@ -1,11 +1,32 @@
 package com.example.backend.controller.user;
 
+import com.example.backend.request.admin.product.ProductRequest;
+import com.example.backend.response.admin.product.ProductDetailResponse;
+import com.example.backend.response.admin.product.ProductResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController("UserCartController")
 @RequestMapping("/api/user/carts")
 @RequiredArgsConstructor
 public class CartController {
+
+    // 一覧取得
+
+
+    // 1件取得
+
+
+    // 登録（作成）
+
+
+    // 更新
+
+
+    // 削除（論理削除）
+
 }
--- backend/src/main/java/com/example/backend/controller/user/OrderController.java
+++ backend/src/main/java/com/example/backend/controller/user/OrderController.java
@@ -8,4 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user/orders")
 @RequiredArgsConstructor
 public class OrderController {
+
+    // 一覧取得
+
+
+    // 1件取得
+
+
+    // 登録（作成）
+
+
+    // 更新
+
+
+    // 削除（論理削除）
 }
